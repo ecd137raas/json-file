3b7dd5bb7bf2e9a362bae589b7e16878f2b3f182
Emerson Duarte <raasjava@gmail.com>
2020-02-22 18:14:51 -0300

    Adicionando express

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
 var connMongoDB = function() {
-	const url = 'mongodb://localhost:27017/ticket';
+	const url = 'mongodb://localhost:27017/importfiles';
 	// Create a new MongoClient
 	const client = new MongoClient(url);
 	// Use connect method to connect to the Server
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var fs = require('fs');
 var Guid = require('guid');
 var uuidHelper = require('mongo-uuid-helper');
+const txtToJSON = require('txt-file-to-json');
 
 module.exports = function(application){
 
     application.get('/', function(req, res){
-        res.render('index');
+        var msg = "";
+        res.render('index', {msg: msg});
     });
 
     application.get('/json', function(req, res){
-        
+        var msg = "";
         var data = fs.readFileSync('./files/synonyms.json');
         var js = JSON.parse(data);	
             js.base.forEach(element => {
@@ -24,7 +26,7 @@ module.exports = function(application){
                 var MongoClient = new application.config.db;
                 MongoClient.connect(function(err, client) {
                     if (err) throw err;
-                    const db = client.db("ticket");
+                    const db = client.db("importfiles");
                     let collection = db.collection('synonyms');
                     collection.insert(strdata).then(result => {
                             }).catch((err) => {
@@ -36,40 +38,37 @@ module.exports = function(application){
                     });
                 });
             }
+            msg = 'Operação finalizada';
          
-        res.redirect('/');
+        res.render('index', {msg: msg});
     });
 
     application.get('/txt', function(req, res){
+        var msg = "";
+        var dataInJSON  = txtToJSON( {filePath: "./files/words.txt", noOfRecords:2});
+        //var data = fs.readFileSync('./files/words.txt', 'utf-8');
+        console.log(dataInJSON);
+        var js = JSON.parse(dataInJSON);
+        console.log(js);
         
-        var data = fs.readFileSync('./');
-        var js = JSON.parse(data);	
-            js.base.forEach(element => {
-                var guid = Guid.create();
-                const csUUID = uuidHelper.csuuidStringToBin(guid.value)
-                element._id=csUUID;
-                element.Title=element.Description
-                insert(element);
-            });
-        
-            function insert(strdata){
-                var MongoClient = new application.config.db;
-                MongoClient.connect(function(err, client) {
-                    if (err) throw err;
-                    const db = client.db("ticket");
-                    let collection = db.collection('synonyms');
-                    collection.insert(strdata).then(result => {
-                            }).catch((err) => {
+        function insert(strdata){
+            var MongoClient = new application.config.db;
+            MongoClient.connect(function(err, client) {
+                if (err) throw err;
+                const db = client.db("ticket");
+                let collection = db.collection('synonyms');
+                collection.insert(strdata).then(result => {
+                        }).catch((err) => {
 
-                                console.log(err);
-                            }).finally(() => {
+                            console.log(err);
+                        }).finally(() => {
 
-                        client.close();
-                    });
+                    client.close();
                 });
-            }
-         
-        res.redirect('/');
+            });
+        }
+       
+        res.render('index', {msg:msg});
     });
 
 }
